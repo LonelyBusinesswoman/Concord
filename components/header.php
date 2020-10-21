ca77043e5d6f5e78fc2f5f1d816434f8e3e4e6dd
Lizaveta <elis.liza567@gmail.com>
2020-10-22 01:07:26 +0300

    fixes

--- components/header.php
+++ components/header.php
@@ -9,9 +9,9 @@
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 	<!-- Main CSS -->
-	<link rel="stylesheet" type="text/css" href="css/style.css?v=5">
+	<link rel="stylesheet" type="text/css" href="css/style.css?v=15">
     <!-- Adapt CSS -->
-    <link rel="stylesheet" type="text/css" href="css/adapt.css?v=5">
+    <link rel="stylesheet" type="text/css" href="css/adapt.css?v=15">
     <!-- slick CSS -->
     <link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
     <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
