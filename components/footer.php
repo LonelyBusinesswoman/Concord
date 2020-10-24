4d7cae94e32e57de01ba623cb0dca5f91af6948d
Lizaveta <elis.liza567@gmail.com>
2020-10-24 11:03:54 +0300

    firefox bug fix

--- components/footer.php
+++ components/footer.php
@@ -40,7 +40,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <!-- main -->
-<script src="js/index.js?v=5"></script>
+<script src="js/index.js?v=8"></script>
 <!-- slick -->
 <script src="assets/slick/slick.min.js"></script>
 <!-- magnific popup -->
