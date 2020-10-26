bb1c0e469e7e23018e97c40180ac3664cc58af36
Lizaveta <elis.liza567@gmail.com>
2020-10-26 14:00:39 +0300

    firefox img fix

--- components/header.php
+++ components/header.php
@@ -9,9 +9,9 @@
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 	<!-- Main CSS -->
-	<link rel="stylesheet" type="text/css" href="css/style.css?v=11">
+	<link rel="stylesheet" type="text/css" href="css/style.css?v=12">
     <!-- Adapt CSS -->
-    <link rel="stylesheet" type="text/css" href="css/adapt.css?v=11">
+    <link rel="stylesheet" type="text/css" href="css/adapt.css?v=12">
     <!-- slick CSS -->
     <link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
     <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
